efada24647ff2d4ffabf11ff2d4071325929f5c9
Emil Mubarqui <emilmubarqui@gmail.com>
2022-08-16 17:59:22 -0500

    Grafica el Salario neto vs bruto

--- ISR.py
+++ ISR.py
@@ -17,8 +17,32 @@ def ISR(Tabla,Salario):
 			ISR = cargoFijo + diferencia*porcentaje
 			return ISR
 
+def grafISR(Tabla):
+	'''
+	Calcula el ISR para un vector de valores con tal de generar una grafica
+	'''
+	Salarios = np.arange(1,100000,1)
+	Impuestos = []
+	SalNeto = []
+
+	for Salario in Salarios:
+		Imp = ISR(Tabla,Salario)
+		print(Salario)
+		Impuestos.append(Imp)
+		SalNeto.append(Salario - Imp)
+
+	Impuestos = np.array(Impuestos)
+	SalNeto = np.array(SalNeto)
+
+	#plt.plot(Salarios,Impuestos)
+	#plt.show()
+
+	plt.plot(Salarios,SalNeto,'m.-',markevery = 5000,ms = 10,lw = 3)
+	plt.xlabel('Salario Bruto')
+	plt.ylabel('Salario Libre')
+	plt.show()
+
+
 Tabla = np.loadtxt('TablaMensual.csv',skiprows=1,delimiter = ',')
 
-Impuesto = ISR(Tabla,30000)
-print(Impuesto)
-
+grafISR(Tabla)
