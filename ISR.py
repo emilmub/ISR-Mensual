9a9acaf7403fccf2643e0bfe14aac511ef5fbf52
Emil Mubarqui <emilmubarqui@gmail.com>
2022-08-16 13:07:35 -0500

    Anadir la funcion de calculo (Falta implementar)

--- ISR.py
+++ ISR.py
@@ -1,3 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def ISR(Tabla,Salario):
+	'''
+	Calcula el Impuesto Sobre la Renta para un salario mensual
+
+	Tabla: Matriz con los datos relevantes para el calculo
+	Salario: Salario mensual para el que deseamos calcular
+	'''
+	continue
